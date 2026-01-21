7b41eb48865756bd61ac7b1928307bd66bbe704c
Espee <atari.m1802476@st.futminna.edu.ng>
2026-01-21 13:51:22 +0100

    Direction Reduction

--- directionReduction.js
+++ directionReduction.js
@@ -1,21 +1,49 @@
+function dirReduct(arrayOfStrings){
+    
+    
+    // function compDir(dir1, dir2){
 
-function compareDir(dir1,dir2){
-    //your dir depends on the functions input
-    //compareDir(NORTH, SOUTH)
+    //     let north = "north", south = "south", east = "east", west = "west";
+    //     let NS, EW
+    //     NS = north && south, NS = false
+    //     EW = east && west, EW = false
 
+    //     //console.log(NS)
+    //     if (NS){
+    //         return null
+    //     } else {
+    //         return dir2
+    //     }
+    // }
+    // compDir("north", "west");
+    
 
-    let north, south, east, west;
-
-    cancelNS = north === south;
-    cancelEW = east === west;
-
-    if (cancelNS || cancelEW){
-        console.log(dir2)
-        return dir2
-    } else {
-        console.log("return null")
-        return null
-    }    
 }
 
-compareDir(north, south)
+let arr1 = ["NORTH", "SOUTH", "EAST", "WEST", "WEST"]
+//dirReduct(arr1)
+
+
+function dirReduc(arr) {
+    const opposites = {
+        'NORTH': 'SOUTH',
+        'SOUTH': 'NORTH',
+        'EAST': 'WEST',
+        'WEST': 'EAST'
+    };
+
+    return arr.reduce((stack, current) => {
+        const last = stack[stack.length - 1];
+        
+        // If the stack isn't empty and the last item is the opposite of current
+        if (stack.length > 0 && last === opposites[current]) {
+            stack.pop(); // Remove the "needless" movement
+        } else {
+            stack.push(current); // Keep the direction
+        }
+        console.log(stack)
+        return stack;
+    }, []);
+}
+
+dirReduc(arr1)
